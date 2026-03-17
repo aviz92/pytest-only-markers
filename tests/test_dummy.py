c5da77ae239628da9c0c234592fd4c2fb3369c09
avizaguri-macbook <avi210892@gmail.com>
2026-03-17 19:30:20 +0200

    Update pytest markers to use dummy_x and dummy_y for testing purposes

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -5,19 +5,19 @@ from custom_python_logger import get_logger, build_logger
 logger = build_logger(__name__)
 
 pytestmark = [
-    pytest.mark.regular,
-    pytest.mark.dummy,
+    pytest.mark.dummy_x,
+    pytest.mark.dummy_y,
 ]
 
-@pytest.mark.ONLY_X
-@pytest.mark.ONLY_Y
+@pytest.mark.ONLY_dummy_x
+@pytest.mark.ONLY_dummy_y
 def test_dummy():
     logger.info("Running test_dummy")
     assert True
 
 
 class TestDummyClass:
-    @pytest.mark.ONLY_X
+    @pytest.mark.ONLY_dummy_x
     def test_dummy_method(self):
         logger.info("Running test_dummy_method")
         assert True
